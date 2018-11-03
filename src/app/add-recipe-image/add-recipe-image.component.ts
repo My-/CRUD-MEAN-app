e0af5417f5d4023fda380803237ca46aa83afc14
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-03 14:09:28 +0000

    first commit( working on add recipie )

---
+++ src/app/add-recipe-image/add-recipe-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-recipe-image',
+  templateUrl: './add-recipe-image.component.html',
+  styleUrls: ['./add-recipe-image.component.css']
+})
+export class AddRecipeImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { AddRecipeImageComponent } from './add-recipe-image/add-recipe-image.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+import { AddRecipeIngredientsComponent } from './add-recipe-ingredients/add-recipe-ingredients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecipeComponent,
+    AddRecipeImageComponent,
+    AddRecipeIngredientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

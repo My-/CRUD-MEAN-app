fe5f1e2742b79ea883df0f4cfa7857d8605cfd25
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-04 22:15:24 +0000

    add user related stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AddRecipeComponent } from './add-recipe/add-recipe.component';
-import { AddRecipeImageComponent } from './add-recipe-image/add-recipe-image.component';
+import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
+import { AddRecipeImageComponent } from './components/add-recipe-image/add-recipe-image.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 
-import { AddRecipeIngredientsComponent } from './add-recipe-ingredients/add-recipe-ingredients.component';
+import { AddRecipeIngredientsComponent } from './components/add-recipe-ingredients/add-recipe-ingredients.component';
+import { UserLoginComponent } from './model/user-login/user-login.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddRecipeComponent,
     AddRecipeImageComponent,
-    AddRecipeIngredientsComponent
+    AddRecipeIngredientsComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/model/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    avatar: string;
+    userName: string;
+    name: string;
+    surname: string;
+    age: number;
+    email: string;
+    phone: number;
+    social: {};
+    location: string;
+}

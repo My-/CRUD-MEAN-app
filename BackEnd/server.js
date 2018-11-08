8693d47b901a4e5ad344f968de9c01220178900c
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-08 13:22:22 +0000

    UPDATED/REFACTORED: to use github & google

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,55 +4,50 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const passport = require('passport')
-const GithubStrategy = require('passport-github').Strategy
-
-const keys = require('../keys/keys')
+// const passport = require('passport')
+const passportSetup = require('./config/passport-setup')
 
 
-passport.use(new GithubStrategy({
-    clientID: keys.GitHub.clientID,
-    clientSecret: keys.GitHub.clientSecret,
-    callbackURL: keys.GitHub.callbackURL
-}, function(accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-}))
+// import routes from auth-routs.js file
+const authRoutes = require('./routes/auth-routs')
+// use routes from auth-routs.js file
+app.use('/auth', authRoutes)
+
+// import mongoose
+const mongoose = require('mongoose')
+const keys = require('../.keys/keys')
+
+console.log(`DB URI: ${keys.usersDB.URI}`)
 
 
-// Express and Passport Session
-const session = require('express-session')
-app.use(session({secret: "-- ENTER CUSTOM SESSION SECRET --"}));
-app.use(passport.initialize());
-app.use(passport.session());
+// connect to mongodb
+mongoose.connect(keys.usersDB.URI, () => {
+    console.log('Connected to MongoDB')
+})
 
-passport.serializeUser(function(user, done) {
-    // placeholder for custom user serialization
-    // null is for errors
-    done(null, user);
-});
 
-passport.deserializeUser(function(user, done) {
-    // placeholder for custom user deserialization.
-    // maybe you are going to get the user from mongo by id?
-    // null is for errors
-    done(null, user);
-});
+// // Express and Passport Session
+// const session = require('express-session')
+// app.use(session({secret: "-- ENTER CUSTOM SESSION SECRET --"}));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // we will call this to start the GitHub Login process
-app.get('/auth/github', passport.authenticate('github'));
+// app.get('/auth/github', passport.authenticate('github'));
 
 // GitHub will call this URL
-app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/'}),
-    function(req, res) {
-    res.redirect('/');
-});
+// app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/'}),
+//     function(req, res) {
+//     res.redirect('/');
+// });
 
 
 // main menu route
 app.get('/', function(req, res) {
     let html = "<ul>\
     <li><a href='/auth/github'>GitHub</a></li>\
-    <li><a href='/logout'>logout</a></li>\
+    <li><a href='/auth/google'>Google</a></li>\
+    <li><a href='/auth/logout'>logout</a></li>\
   </ul>"
 
     // dump the user for debugging
@@ -67,11 +62,11 @@ app.get('/', function(req, res) {
 });
 
 
-app.get('/logout', function(req, res) {
-    console.log('logging out');
-    req.logout();
-    res.redirect('/');
-});
+// app.get('/logout', function(req, res) {
+//     console.log('logging out');
+//     req.logout();
+//     res.redirect('/');
+// });
 
 
 const server = app.listen(port, function() {

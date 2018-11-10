843797a44746685cdce9261306125efde794f5cd
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-10 22:35:10 +0000

    save to db recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,17 +1,23 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
 import {Recipe} from '../model/recipe';
 import {Observable} from 'rxjs';
 
 @Injectable()
 export class RecipeService {
 
-    constructor() {
+    constructor(private _http: HttpClient) {
     }
 
 
     add(userRecipe: Recipe): Observable<any> {
         // TODO: add recipe to DB
-        console.log(userRecipe);
-        return Observable.create(sub => sub.next(userRecipe));
+        console.log(`Sending to server: ${JSON.stringify(userRecipe, null, 4)}`);
+
+        const msg = {message: 'hello'};
+
+        return this._http.post('http://localhost:3000/recipe/save', userRecipe);
+        // return Observable.create(sub => sub.next(userRecipe));
     }
 }

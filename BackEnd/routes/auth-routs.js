83e8d5a43951e4d47dbc687c95375cbb80c313d2
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-10 00:18:17 +0000

    REFACTOR: returning response insted of printing it

--- BackEnd/routes/auth-routs.js
+++ BackEnd/routes/auth-routs.js
@@ -10,7 +10,9 @@ router.get('/login', (req, res) => {
 // auth logout
 router.get('/logout', (req, res) => {
     // handle with passport
-    res.send('Logging out')
+    console.log('Logging out...') // <------------- DEBUG
+    return res.status(200).json({message:'Logout Success'})
+    // res.send('Logging out')
 })
 
 /* GOOGLE */
@@ -33,12 +35,13 @@ router.get('/github', passport.authenticate('github', {
     scope: ['user']
 }), (req, res) => {
     // handle with passport
-    res.send('Logging in with GinHub')
+    return res.status(200).json({message:'Logging in with GinHub'})
 })
 
 // github call back URI
 router.get('/github/callback', passport.authenticate('github'), (req, res) => {
-    res.send("<pre>" + JSON.stringify(req.user, null, 4) + "</pre>")
+    console.log(JSON.stringify(req.user, null, 4))
+    // res.send("<pre>" + JSON.stringify(req.user, null, 4) + "</pre>")
 })
 
 module.exports = router

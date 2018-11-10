e44c6bc56a5b16c204db500e39299baf13736f70
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-10 00:34:36 +0000

    ADD: implemented some user functionality ( login github, google). ADD: service user TODO: fix CORS error

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {LoginService} from '../../services/login.service';
 
 @Component({
     selector: 'app-user-login',
@@ -9,26 +10,42 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class UserLoginComponent implements OnInit {
 
-    loginForm: FormGroup =  new FormGroup({
+    loginForm: FormGroup = new FormGroup({
         email: new FormControl(null, [Validators.email, Validators.required]),
         password: new FormControl(null, Validators.required)
     });
 
-    constructor(private _router: Router) {
+    hide: boolean = true;
+
+    constructor(private _router: Router, private _loginService: LoginService) {
     }
 
     ngOnInit() {
     }
 
-    goToRgister(){
+    goToRgister() {
         this._router.navigate(['/register']);
     }
 
     login() {
         if (!this.loginForm.valid) {
-            console.log('Invalid Login'); return;
+            console.log('Invalid Login');
+            return;
         }
 
         console.log(JSON.stringify(this.loginForm.value));
+    }
+
+    logout() {
+        this._loginService.logout().subscribe(res => console.log(res));
+
+    }
+
+    loginGitHub() {
+        this._loginService.github().subscribe(res => console.log(res));
+    }
+
+    loginGoogle() {
+        this._loginService.google().subscribe(res => console.log(res));
     }
 }

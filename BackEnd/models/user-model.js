2e3d383d4e1201f09e920644cd02acc71af9c777
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-10 00:12:29 +0000

    REFACTOR: changed file name, moved db connection to this file

--- BackEnd/models/user-model.js
+++ BackEnd/models/user-model.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const keys = require('../../.keys/keys')
+
+// connect to mongodb Users
+const userDB = mongoose.createConnection(keys.usersDB.URI, () => {
+    console.log('Connected to MongoDB: Users')
+})
 
 // schema for user model
 const userSchema = new Schema({
@@ -12,7 +18,7 @@ const userSchema = new Schema({
 })
 
 // user model
-const User = mongoose.model('user', userSchema)
+const User = userDB.model('user', userSchema)
 
 // export user model
 module.exports = User

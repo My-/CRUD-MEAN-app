389dac29324f961a5de1edb2ebac4b9111bcf33b
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-10 00:36:21 +0000

    ADD: some add recipe functionality. Work still in progress.

--- src/app/components/add-recipe/add-recipe.component.ts
+++ src/app/components/add-recipe/add-recipe.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {RecipeService} from '../../services/recipe.service';
+import {Recipe} from '../../model/recipe';
+import {AddRecipeIngredientsComponent} from '../add-recipe-ingredients/add-recipe-ingredients.component';
 
 
 @Component({
-  selector: 'app-add-recipe',
-  templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.css']
+    selector: 'app-add-recipe',
+    templateUrl: './add-recipe.component.html',
+    styleUrls: ['./add-recipe.component.css']
 })
+
+/**
+ * This component responsible for adding new recipes
+ */
 export class AddRecipeComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild(AddRecipeIngredientsComponent) recipeIngredients: AddRecipeIngredientsComponent;
 
-  ngOnInit() {
-  }
+    constructor(private _recipeDB: RecipeService) {
+    }
 
+    ngOnInit() {
+    }
+
+
+    onAddRecipe(form: NgForm) {
+        console.log(form.value);
+
+        const userRecipe: Recipe = {
+            userID: 0,
+            title: form.value.title,
+            allergies: [],
+            takesTime: 0,
+            pictures: [],
+            ingredients: this.recipeIngredients.ingredients,
+            recipe: form.value.recipe,
+        };
+
+        this._recipeDB.add(userRecipe).subscribe(res => console.log(res));
+
+        form.resetForm();
+    }
 }

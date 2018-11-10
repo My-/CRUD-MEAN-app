550f12731dd74900dcd79492ae932e789b4101b4
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-10 00:28:18 +0000

    ADD: recipe igrediens works, can create list of igredients. REMOVED: old unused code.

--- src/app/components/add-recipe-ingredients/add-recipe-ingredients.component.ts
+++ src/app/components/add-recipe-ingredients/add-recipe-ingredients.component.ts
@@ -1,23 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import {Sort} from '@angular/material';
-
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort, MatTableDataSource} from '@angular/material';
 import {Ingredient} from '../../model/ingredient';
+import {NgForm} from '@angular/forms';
+import {Recipe} from '../../model/recipe';
 
 @Component({
     selector: 'app-add-recipe-ingredients',
     templateUrl: './add-recipe-ingredients.component.html',
     styleUrls: ['./add-recipe-ingredients.component.css']
 })
-
-
 export class AddRecipeIngredientsComponent implements OnInit {
-    ingredients: Ingredient[] = [
-        {name: 'Frozen yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
-        {name: 'Ice cream sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
-        {name: 'Eclair', calories: 262, fat: 16, carbs: 24, protein: 6},
-        {name: 'Cupcake', calories: 305, fat: 4, carbs: 67, protein: 4},
-        {name: 'Gingerbread', calories: 356, fat: 16, carbs: 49, protein: 4},
+
+    public ingredients: Ingredient[] = [
+        {name: 'Frozen yogurt', amount: 3},
+        {name: 'Ice cream sandwich', amount: 5},
+        {name: 'Eclair', amount: 7},
+        {name: 'Cupcake', amount: 11},
+        {name: 'Gingerbread', amount: 13},
     ];
+
+    displayedColumns: string[] = ['name', 'amount'];
+    dataSource = new MatTableDataSource(this.ingredients);
+
+    @ViewChild(MatSort) sort: MatSort;
 
     sortedData: Ingredient[];
 
@@ -25,37 +30,24 @@ export class AddRecipeIngredientsComponent implements OnInit {
         this.sortedData = this.ingredients.slice();
     }
 
-    sortData(sort: Sort) {
-        const data = this.ingredients.slice();
-        if (!sort.active || sort.direction === '') {
-            this.sortedData = data;
-            return;
-        }
-
-        this.sortedData = data.sort((a, b) => {
-            const isAsc = sort.direction === 'asc';
-            switch (sort.active) {
-                case 'name':
-                    return compare(a.name, b.name, isAsc);
-                case 'calories':
-                    return compare(a.calories, b.calories, isAsc);
-                case 'fat':
-                    return compare(a.fat, b.fat, isAsc);
-                case 'carbs':
-                    return compare(a.carbs, b.carbs, isAsc);
-                case 'protein':
-                    return compare(a.protein, b.protein, isAsc);
-                default:
-                    return 0;
-            }
-        });
-    }
-
     ngOnInit() {
+        this.dataSource.sort = this.sort;
     }
 
-}
+    onAddIngredient(form: NgForm) {
+        console.log(form.value);
 
-function compare(a, b, isAsc) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+        const recipeIngredient: Ingredient = {
+            name: form.value.ingredient,
+            amount: form.value.amount,
+        };
+
+        this.ingredients.push(recipeIngredient);
+
+        console.log(this.ingredients);
+        this.ngOnInit();
+
+        form.resetForm();
+    }
+
 }

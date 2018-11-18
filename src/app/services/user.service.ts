76de247e9704901573571d1070d6650e265dad2a
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-18 22:12:52 +0000

    IMPLEMENTED: user.service login()

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,46 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+import {LoggedUser, User} from '../model/user';
+
+import {Observable} from 'rxjs';
+import {throwError} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+
 @Injectable()
 
 export class UserService {
 
     constructor(private _http: HttpClient) {
+    /**
+     * Logs user in.
+     *      - post user to express,
+     *          - express validates user,
+     *          - if valid, express sends back user + JWT,
+     *      - saves JWT to local storage,
+     *      - saves user to LoggedUser
+     *
+     * @param user user to login
+     */
+    login({user}): Observable<User> {
+        // validate if required values exist
+        if (!user || !user.username || !user.password) {
+            return throwError({
+                message: `Missing data... user:  ${JSON.stringify(user, null, 4)}`,
+            });
+        }
+
+        // send post request to express server
+        return this._http.post('http://localhost:3000/auth/localLogin', user)
+                .pipe(
+                    // log response
+                    tap(val => console.log(`...got ${JSON.stringify(val, null, 4)}`)),
+                    // save JWT to local storage
+                    tap(val => localStorage.setItem(LoggedUser.localStorageJWT, (val as any).token)),
+                    // map to User object
+                    map(val => <User>(val as any).user),
+                    // save logged user as saved user
+                    tap(val => LoggedUser.set(val)),
+                );
     }
 }

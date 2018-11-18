1e0e124c3ae48498b64eada684fccdaf51d86e3f
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-18 22:08:34 +0000

    ADD: hash passwords with bcryptjs. ADD: methods to veryfy/hash passwords. REFACTOR: user model.

--- BackEnd/models/user-model.js
+++ BackEnd/models/user-model.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs');
+
 const keys = require('../../.keys/keys')
+const LoginMethods = require('../routes/loginMethods').LoginMethods
 
 // connect to mongodb Users
 const userDB = mongoose.createConnection(keys.usersDB.URI, () => {
@@ -9,13 +12,41 @@ const userDB = mongoose.createConnection(keys.usersDB.URI, () => {
 
 // schema for user model
 const userSchema = new Schema({
-    userName: String,
+    userName: {
+        type: String,
+        unique: true,
+        required: true
+    }, // String,
     loginMethod: String,
-    loginId: Number,
+    password: {
+        type: String,
+        required: () => this.loginMethod === LoginMethods.LOCAL,
+    },
     avatar: String,
+    gender: {
+        type: String,
+        default: 'other',
+    },
     profile: Object,
     created: { type: Date, default: Date.now },
 })
+
+userSchema.statics.hashPassword = function hashPassword(password){
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    // Store hash in your password DB.
+    return hash;
+}
+
+userSchema.methods.isValid = function(hashedpassword){
+    return bcrypt.compareSync(hashedpassword, this.password);
+}
+
+userSchema.methods.verifyPassword = function(password){
+    // Load hash from your password DB.
+    const hash = this.password
+    return bcrypt.compareSync(password, hash)
+}
 
 // user model
 const User = userDB.model('user', userSchema)

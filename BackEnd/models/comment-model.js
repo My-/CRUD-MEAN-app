f569b7dfd72f3f840d6d90860cbb8d6a2c9260f4
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-20 18:25:37 +0000

    CREATED: mongoose comment model

---
+++ BackEnd/models/comment-model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const validate = require('mongoose-validator')
+
+const keys = require('../../.keys/keys')
+
+// connect to mongodb
+const DB = mongoose.createConnection(keys.usersDB.URI, () => {
+    console.log('Connected to MongoDB: Comments')
+})
+
+// schema for comment model
+const commentSchema = new Schema({
+    userID: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+        validate: validate({
+            validator: 'isLength',
+            arguments: [10, 1000],
+            message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
+        })
+    },
+    responds: [{commentID: String}],
+    created: { type: Date, default: Date.now },
+    edited: { type: Date },
+})
+
+// user model
+const Comments = DB.model('user', commentSchema)
+
+// export user model
+module.exports = Comments

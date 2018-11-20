8c191dca8ac6374878986da412d103a9a937b7b7
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-20 15:50:34 +0000

    CHANGE: rewrited how add recipe to DB works

--- BackEnd/routes/recipe-routs.js
+++ BackEnd/routes/recipe-routs.js
@@ -1,29 +1,52 @@
 const router = require('express').Router()
 const Recipe = require('../models/recipe-model')
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const jwt = require('jsonwebtoken')
+
+const keys = require('../../.keys/keys')
 
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json())
 
 // add recipe route
-router.post('/save', (req, res) => {
-    console.log("post successful");
-    // console.log(req.headers)
-    console.log(req.body)
+router.post('/', (req, res) => {
+    /**
+     * 1. Validate JWT
+     *      true  -> set userID to id from JWT users id
+     *      false -> set userID to null
+     * 2. Create recipe from request body with validated userID
+     * 3. Save to DB
+     * 4. Send back response.
+     */
+    Promise.resolve(req)
+        .then(data => ExtractJwt.fromAuthHeaderAsBearerToken()(data))
+        .then(jwtToken => jwt.verify(jwtToken, keys.JWT.secret))
+        .then(tokenData => tokenData.payload.id)
+        .catch(() =>  null )
+        // userID === null  -> anonymous user
+        // userID !== null  -> verified user
+        .then(userID => createRecipe({userID, recipe: req.body}))
+        .then(recipe => recipe.save())
+        .then(newRecipe => res.json(200, newRecipe))
+        .catch(err => res.json(400, {err}))
 
-    new Recipe({
-        userID: req.body.userID,
-        title: req.body.title,
-        allergies: req.body.allergies,
-        takesTime: req.body.takesTime,
-        pictures: req.body.pictures,
-        ingredients: req.body.ingredients,    // Ingredient object
-        recipe: req.body.recipe,
-    }).save().then(newRecipe => {
-        console.log(`
-        Created recipe: ${newRecipe}`)
-        return res.status(200).json(newRecipe)
-    })
+
+    /**
+     * Creates recipe from request
+     * @param userID - id of the user who creates recipe
+     * @param recipe - recipe data
+     */
+    function createRecipe({userID, recipe}) {
+        return new Recipe({
+            userID,
+            title: recipe.title,
+            allergies: recipe.allergies,
+            takesTime: recipe.takesTime,
+            pictures: recipe.pictures,
+            ingredients: recipe.ingredients,    // Ingredient object
+            recipe: recipe.recipe,
+        })
+    }
+
 })
 
 // get recipe from DataBase

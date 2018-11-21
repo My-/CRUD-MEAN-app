99b8fbe19cadae68fbb9fd2c52a0bc483daeb3a1
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-11-21 20:22:41 +0000

    FIX, REFACTOR: add user works

--- BackEnd/routes/users-routs.js
+++ BackEnd/routes/users-routs.js
@@ -3,14 +3,14 @@ const router = require('express').Router()
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 
-const User = require('../models/user-model')
+const UserModel = require('../models/user-model')
 const keys = require('../../.keys/keys')
 require('../config/passport-setup')
 
 
-// get user owning, liked, saved comments
+// get user comments
 router.get('/comments', passport.authenticate('jwt', {session: false}), (req, res, next) => {
-    User.findById(req.user.id, [`comments`])
+    UserModel.findById(req.user.id, [`comments`])
         .then(val => res.status(200).json(val))
         .catch(err => res.status(400).json({err}))
 })
@@ -25,7 +25,7 @@ router.get('/recipes', passport.authenticate('jwt', {session: false}), (req, res
             return res.status(404).json({message: `Not allowed parameter(s): [${req.query.type}]`})
     }
 
-    User.findById(req.user.id, [`recipes.${req.query.type}`])
+    UserModel.findById(req.user.id, [`recipes.${req.query.type}`])
         .then(val => res.status(200).json(val.recipes))
         .catch(err => res.status(400).json({err}))
 })
@@ -34,7 +34,7 @@ router.get('/recipes', passport.authenticate('jwt', {session: false}), (req, res
 router.put('/', passport.authenticate('jwt', {session: false}), (req, res, next) => {
     const resObj = {}   // response object
     // update user and build response object
-    User.findByIdAndUpdate(req.user.id, req.body)
+    UserModel.findByIdAndUpdate(req.user.id, req.body,)// {safe: true, new : true})
         // write before update user to response object
         .then(user => {
             resObj.message = 'Update OK!'
@@ -44,7 +44,7 @@ router.put('/', passport.authenticate('jwt', {session: false}), (req, res, next)
         .catch(err => resObj.errBeffore = err)
         // write after update user to response object
         .then(() =>
-            User.findById(req.user.id)
+            UserModel.findById(req.user.id)
                 .then(user => resObj.after = user)
                 .catch(err => console.log(err))
         )
@@ -56,52 +56,39 @@ router.put('/', passport.authenticate('jwt', {session: false}), (req, res, next)
 
 // delete (kill) user. JWT protected.
 router.delete('/', passport.authenticate('jwt', {session: false}), (req, res, next) => {
-    User.findByIdAndRemove(req.user._id)
+    UserModel.findByIdAndRemove(req.user._id)
         .then(user => res.json({message: 'Deleted! ', user}))
         .catch(err => console.log(err))
 })
 
-// register user route
+// Create new user
 router.post('/register', (req, res, next) => {
-    console.log(req.body.userName)
 
-    User.findOne({ userName: req.body.userName })
-        // Check if user exist in db
-        .then(val => {
-            console.log('userCheck:')
-            console.log(val)
-            console.log(req.body)
-            // User exist. https://stackoverflow.com/a/3826024/5322506
-            if( val ){
-                throw res.status(409).json({message: `User ("${req.body.userName}") already exist!`})
-            }
-        })
-        // User name do NOT exist. Create one.
-        .then(() => new User({
-                userName: req.body.userName,
-                password: User.hashPassword(req.body.password),
-                gender: req.body.gender,
-                avatar: req.body.avatar,
+    new Promise((resolve, reject) => {
+        if( !req.body.userName ){ reject('body.userName is required.') }
+        else if( !req.body.password ){ reject('body.password is required.') }
+        else{
+            UserModel.findOne({ userName: req.body.userName })
+                .then(userExist => {
+                    if( userExist ){
+                        reject({message: `User: ${req.body.userName} already exist!`})
+                    }
+                    else{ resolve() }
+                })
+        }
+    })
+        // create user
+        .then(() => new UserModel({
+            userName: req.body.userName,
+            password: UserModel.hashPassword(req.body.password),
+            gender: req.body.gender,
+            avatar: req.body.avatar,
         }))
         // Save user to DB
         .then(newUser => newUser.save())
-        // show saved user to console
-        .then(newUser => { console.log(`Saved user: ${newUser}`); return newUser; } )
-        // get users info for JWT token
-        .then(savedUser => {return {
-            id: savedUser._id,
-            userName: savedUser.userName,
-            gender: savedUser.gender,
-            avatar: savedUser.avatar,
-        }})
-        // create JWT token
-        .then(userInfo => {
-            res.status(200).json({
-                user: userInfo,
-                JWT: jwt.sign({userInfo}, keys.JWT.secret, { expiresIn: '1h' }, ),
-            })
-        })
-        .catch(err => console.log(err))
+        // send response
+        .then(savedUser => res.status(200).json(savedUser))
+        .catch(err => res.status(400).json({err}))
 })
 
 
